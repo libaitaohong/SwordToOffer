217b318489d935289dad14999a55f3603d3d4e99
libaitaohong <492563822@qq.com>
2016-12-08 17:30:23 +0800

    最小的k个数

--- src/GetLeastNumbers_Solution.java
+++ src/GetLeastNumbers_Solution.java
@@ -6,36 +6,26 @@ import java.util.ArrayList;
  * 时间: 2016/12/8 17:07
  * 题目:最小的k个数
  * 内容:输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
- * 版本:
- * 运行时间：36ms 占用内存：629k
+ * 版本:2
+ * 运行时间：运行时间：35ms 占用内存：629k
  * 备注:注意边界条件。k是不是大于0，还有数组的长度是不是大于k的长度。若大于返回空。
+ * 简单修改了一下，去掉判断input大小=k的情况，取消数组arr，直接在input数组的前k项进行变换
  */
 public class GetLeastNumbers_Solution {
     public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
         ArrayList<Integer> res = new ArrayList<>();
         if(input.length < k || k <=0)
             return res;
-        else if(input.length == k){
-            for(int n : input)
-                res.add(n);
-            return res;
-        }
-        int[] arr = new int[k];
-        for(int i = 0; i < k; i++)
-            arr[i] = input[i];
-
         //构造大顶堆,构造完之后数组第一个值为最大值
         for(int j = k; j < input.length; j++){
-            for(int i = arr.length/2-1; i >= 0; i--){
-                transform(arr, i, arr.length-1);
+            for(int i = k/2-1; i >= 0; i--){
+                transform(input, i, k-1);
             }
-            if(arr[0] > input[j])
-                arr[0] = input[j];
+            if(input[0] > input[j])
+                input[0] = input[j];
         }
-
-        for(int n : arr)
-            res.add(n);
-
+        for(int n = 0; n < k; n++)
+            res.add(input[n]);
         return res;
     }
     public void transform(int[] a, int i, int n){
