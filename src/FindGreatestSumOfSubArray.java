d5fade1245f095cb6793a84209733e586cf7c87a
libaitaohong <492563822@qq.com>
2016-12-08 21:28:47 +0800

    连续子数组的最大和

--- src/FindGreatestSumOfSubArray.java
+++ src/FindGreatestSumOfSubArray.java
@@ -4,9 +4,12 @@
  * 时间: 2016/12/8 21:13
  * 题目:连续子数组的最大和
  * 内容:
- * 版本:1
- * 运行时间：28ms占用内存：688k
+ * 版本:2
+ * 运行时间：运行时间：35ms 占用内存：654k
  * 备注:要注意全为负数的情况，Integer.MIN_VALUE是整形数的最小值
+ * 使用dp来解决这个问题
+ * 如果第i-1个位置的累加大于等于0，则与第i个位置数相加，否则让累加等于第i个数的值
+ * 此时的max与count的较大值作为max的值
  */
 public class FindGreatestSumOfSubArray {
     public int FindGreatestSumOfSubArray(int[] array) {
@@ -14,13 +17,8 @@ public class FindGreatestSumOfSubArray {
         int count = 0;
         int max = Integer.MIN_VALUE;
         for(int i = 0; i < array.length; i++){
-            if(count <= 0)
-                count = array[i];
-            else
-                count +=array[i];
-            if(count > max)
-                max = count;
-
+            count = (count >= 0) ? count+array[i] : array[i];
+            max = (count > max) ? count : max;
         }
         return max;
     }
